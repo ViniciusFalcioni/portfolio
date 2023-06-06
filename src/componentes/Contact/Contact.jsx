22bd7018bfc9fc1d63c0ba63f71767bea5279aa0
Vinicius Falcioni <viniciuskrilow@gmail.com>
2023-06-06 01:54:18 -0300

    Commit 4 - Form Configurar

--- src/componentes/Contact/Contact.jsx
+++ src/componentes/Contact/Contact.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ExpandCircleDownOutlined, Mail, WhatsApp, LinkedIn, Instagram, GitHub } from '@mui/icons-material';
+import emailjs from '@emailjs/browser'
 
 export default function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    function sendEmail(event) {
+        event.preventDefault();
+
+        if (name === '' || email === '' || message === "") {
+            alert("Preencha todos os campos");
+            return;
+        }
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+
+        emailjs.send("service_ixllsv5", "template_m037okt", templateParams, "De8YXV6Rzu5dOvPCY")
+            .then((response) => {
+                console.log("Email enviado com sucesso!", response.status, response.text)
+                setName('')
+                setEmail('')
+                setMessage('')
+            }, (err) => {
+                console.log("Erro: ", err)
+            })
+    }
+
     return (
         <div id='contact' className='bg-customBlue text-white'>
             <div className='container mx-auto font-rubik px-4 pt-16 py-10 overflow-hidden'>
@@ -14,18 +44,18 @@ export default function Contact() {
                     <div className='flex flex-col items-center justify-center gap-8 mt-8 text-white'>
 
 
-                        <form className='flex flex-col gap-6 w-full' action="">
+                        <form className='flex flex-col gap-6 w-full' onSubmit={sendEmail} >
                             <div className="flex flex-col">
                                 <label htmlFor="input" className="text-white mb-2">Nome:</label>
-                                <input type="text" id="input" className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
+                                <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
                             </div>
                             <div className="flex flex-col">
                                 <label htmlFor="input" className="text-white mb-2">E-mail:</label>
-                                <input type="email" id="input" className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
+                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
                             </div>
                             <div className="flex flex-col">
                                 <label htmlFor="input" className="text-white mb-2">Mensagem:</label>
-                                <textarea type="text" id="input" className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
+                                <textarea type="text" value={message} onChange={(e) => setMessage(e.target.value)} className="w-full lg:w-96 border border-gray-300 rounded p-2 text-gray-500" placeholder="Digite aqui" />
                             </div>
                             <button className='bg-customBlueClear p-2 hover:scale-105' type='submit'>Enviar</button>
                         </form>
